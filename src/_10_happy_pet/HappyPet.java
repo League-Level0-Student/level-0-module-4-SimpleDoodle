ecb1c69efb9b44c03f717a72b3514084a3dcb531
League Student <league@iMac-7.attlocal.net>
2019-09-19 18:31:47 -0700

    :laughing:

--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -30,7 +30,39 @@ public class HappyPet {
 		// 6. If you determine the happiness level is large enough, tell the
 		// user that he loves his pet and use break; to exit for loop.
 		if (Pet.equalsIgnoreCase("Rock")) {
-
+			if (task == 0) {
+				JOptionPane.showMessageDialog(null, "Your " + Pet + " has died.");
+			}
+			else if(task==1) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You cuddle with your " + Pet + ". Your " + Pet + "'s affection is raised to level." + happinessLevel);
+				
+			}
+			else if(task==2) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You feed your " + Pet + " diet air. Your " + Pet + "'s affection is raised to level." + happinessLevel);
+				
+			}
+			else if(task==3) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You watered your" + Pet + ", it's now wet. Your " + Pet + "'s affection is raised to level." + happinessLevel);
+				
+			}
+			else if(task==4) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You walk your " + Pet + " and it has a great time. Your " + Pet + "'s affection is raised to level." + happinessLevel);
+				
+			}
+			else if(task==5) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You groom your " + Pet + "it was covered in dirt. Your " + Pet + "'s affection is raised to level." + happinessLevel);
+				
+			}
+			else if(task==6) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You clean up your " + Pet + "'s dust. Your " + Pet + "'s affection is raised to level." + happinessLevel);
+				
+			}
 		}
 		if (Pet.equalsIgnoreCase("Dog")) {
 			if (task == 0) {
@@ -38,13 +70,70 @@ public class HappyPet {
 			}
 			else if(task==1) {
 				happinessLevel ++;
-				JOptionPane.showMessageDialog(null, "Your dog's affection is raised to level " + happinessLevel);
+				JOptionPane.showMessageDialog(null, "You cuddle with your dog it's very furry. Your dog's affection is raised to level " + happinessLevel);
+				
+			}
+			else if(task==2) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You feed your dog a porkchop. Your dog's affection is raised to level " + happinessLevel);
+				
+			}
+			else if(task==3) {
+				happinessLevel --;
+				JOptionPane.showMessageDialog(null, "You watered your dog, it's now wet. Your dog's affection is lowered to level " + happinessLevel);
+				
+			}
+			else if(task==4) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You walk your dog and it has a great time. Your dog's affection is raised to level " + happinessLevel);
+				
+			}
+			else if(task==5) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You groom your dog, it's fur was covered in dirt. Your dog's affection is raised to level " + happinessLevel);
+				
+			}
+			else if(task==6) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You clean up your dog's poop. Your dog's affection is raised to level " + happinessLevel);
 				
 			}
 		}
 
 		if (Pet.equalsIgnoreCase("Tardigrade")) {
-
+			if (task == 0) {
+				JOptionPane.showMessageDialog(null, "Your " + Pet + " has died.");
+			}
+			else if(task==1) {
+				happinessLevel --;
+				JOptionPane.showMessageDialog(null, "You cuddle with your " + Pet + ". Your " + Pet + "'s affection is lowered to level." + happinessLevel);
+				
+			}
+			else if(task==2) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You feed your " + Pet + " pellets. Your " + Pet + "'s affection is raised to level." + happinessLevel);
+				
+			}
+			else if(task==3) {
+				happinessLevel --;
+				JOptionPane.showMessageDialog(null, "You watered your" + Pet + ", it's now wet. Your " + Pet + "'s affection is lowered to level." + happinessLevel);
+				
+			}
+			else if(task==4) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You walk your " + Pet + " and it has a great time. Your " + Pet + "'s affection is raised to level." + happinessLevel);
+				
+			}
+			else if(task==5) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You groom your " + Pet + "it's body was covered in particles. Your " + Pet + "'s affection is raised to level." + happinessLevel);
+				
+			}
+			else if(task==6) {
+				happinessLevel ++;
+				JOptionPane.showMessageDialog(null, "You clean up your " + Pet + "'s poop. Your " + Pet + "'s affection is raised to level." + happinessLevel);
+				
+			}
 		}
 		if (Pet.equalsIgnoreCase("Flower")) {
 
